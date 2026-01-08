e09994e0cd50c40ab6b778bdee801fe40530590e
marcotristani <marco.tristani@hotmail.it>
2026-01-08 14:55:09 +0100

    flat: creato collegamento API con html

--- js/script.js
+++ js/script.js
@@ -7,9 +7,28 @@ const endpoint = "https://lanciweb.github.io/demo/api/pictures/";
 // Chiamata ajax
 axios.get(endpoint)
 .then(response => {
+    //prendo l'array di oggetti che mi serev dalla chiamata
     const posts = response.data;
-    console.log(posts);
     
+    //Creo una stringa vuota dove andare ad incrementare il testo html ogni volta che prendo i dati che mi servono dall'oggetto dell'array
+    let innerCard = "";
+    //Faccio un ciclo per attraversare tutti gli oggetti dell'array
+    posts.forEach(post => {
+    //destrutturo l'array
+    const { title, date, url : urlImg } = post;
+    innerCard += `       
+        <div class="card">
+          <img src="./img/pin.svg" alt="" id="pin" />
+          <figure>
+            <img src="${urlImg}" alt="" />
+          </figure>
+          <figcaption>
+            <div>${title}</div>
+            <div>${date}</div>
+          </figcaption>
+        </div>`
+    });
+    outputCard.innerHTML = innerCard;
     
 
 })
