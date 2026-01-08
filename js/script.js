2449da2f852c6a3efb180cb62d14c907423d932a
marcotristani <marco.tristani@hotmail.it>
2026-01-08 15:23:50 +0100

    style: adattato padding laterale in base al vw

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
 // collegamento riferimento file html
-const outputCard = document.getElementById('container');
+const outputCard = document.getElementById("container");
 
-// definizione endpoint di riferimento all'url dell'API 
+// definizione endpoint di riferimento all'url dell'API
 const endpoint = "https://lanciweb.github.io/demo/api/pictures/";
 
 // Chiamata ajax
-axios.get(endpoint)
-.then(response => {
+axios
+  .get(endpoint)
+  .then((response) => {
     //prendo l'array di oggetti che mi serev dalla chiamata
     const posts = response.data;
-    
+
     //Creo una stringa vuota dove andare ad incrementare il testo html ogni volta che prendo i dati che mi servono dall'oggetto dell'array
     let innerCard = "";
     //Faccio un ciclo per attraversare tutti gli oggetti dell'array
-    posts.forEach(post => {
-    //destrutturo l'array
-    const { title, date, url : urlImg } = post;
-    innerCard += `       
+    posts.forEach((post) => {
+      //destrutturo l'array
+      const { title, date, url: urlImg } = post;
+      innerCard += `       
         <div class="card">
           <img src="./img/pin.svg" alt="" id="pin" />
           <figure>
@@ -26,14 +27,11 @@ axios.get(endpoint)
             <div>${title}</div>
             <div>${date}</div>
           </figcaption>
-        </div>`
+        </div>`;
     });
     outputCard.innerHTML = innerCard;
-    
-
-})
-.catch( error => {
-        // codice da eseguire in caso di errore
-        console.error(error.message)
-      })
-
+  })
+  .catch((error) => {
+    // codice da eseguire in caso di errore
+    console.error(error.message);
+  });
