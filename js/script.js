b21c0774d8e715b178cbfd5b0484b7bd686555c2
marcotristani <marco.tristani@hotmail.it>
2026-01-08 17:08:13 +0100

    refactoring: definita funzione per testo html e stampato a schermo messaggio di errore

--- js/script.js
+++ js/script.js
@@ -14,24 +14,31 @@ axios
     //Creo una stringa vuota dove andare ad incrementare il testo html ogni volta che prendo i dati che mi servono dall'oggetto dell'array
     let innerCard = "";
     //Faccio un ciclo per attraversare tutti gli oggetti dell'array
-    posts.forEach((post) => {
+    posts.forEach(post => {
       //destrutturo l'array
-      const { title, date, url: urlImg } = post;
-      innerCard += `       
-        <div class="card">
-          <img src="./img/pin.svg" alt="" id="pin" />
-          <figure>
-            <img src="${urlImg}" alt="" />
-          </figure>
-          <figcaption>
-            <div>${title}</div>
-            <div>${date}</div>
-          </figcaption>
-        </div>`;
+      const { title, date, url } = post;
+      //faccio chiamata alla funzione per avere il testo da inserire nell'html
+      innerCard += cardGenerator ( title, date, url);
     });
     outputCard.innerHTML = innerCard;
   })
   .catch((error) => {
-    // codice da eseguire in caso di errore
-    console.error(error.message);
+    // codice da eseguire in caso di errore(mando errore in pagina)
+    document.querySelector('body').innerHTML = error.message;
   });
+
+
+  function cardGenerator (description, date, urlImage) {
+        output =`       
+        <div class="card">
+          <img src="./img/pin.svg" alt="" id="pin" />
+          <figure>
+            <img src="${urlImage}" alt="" />
+          </figure>
+          <figcaption>
+            <div>${description}</div>
+            <div>${date}</div>
+          </figcaption>
+        </div>`;
+        return output;
+    };
