58be1b66de4a3e18cbea40b0565892907b97bebd
marcotristani <marco.tristani@hotmail.it>
2026-01-11 12:47:29 +0100

    refactoring : ottimizzato e commentato codice

--- js/script.js
+++ js/script.js
@@ -16,49 +16,48 @@ axios
     //Creo una stringa vuota dove andare ad incrementare il testo html ogni volta che prendo i dati che mi servono dall'oggetto dell'array
     let innerCard = "";
     //Faccio un ciclo per attraversare tutti gli oggetti dell'array
-    posts.forEach((post) => {
+    posts.forEach( post => {
       //destrutturo l'array
       const { title, date, url } = post;
       //faccio chiamata alla funzione per avere il testo da inserire nell'html
       innerCard += cardGenerator(title, date, url);
     });
     outputCard.innerHTML = innerCard;
-    console.log(posts);
 
     //Prendiamo riferimenti card della pagina(creando un array con tutti i riferimenti ad ogni singola card)
     const allCard = document.querySelectorAll(".card");
-    console.log(allCard);
 
-    //selezioniamo l'evento di click su una card
-    for (let index = 0; index < allCard.length; index++) {
-      allCard[index].addEventListener("click", (event) => {
-        activateOverlay.classList.remove("disactive");
-        stopBody.classList.add("hidden");
-        const selectedCard = posts.find(post => post.id === index + 1);
-        activateOverlay.innerHTML = overlayGenerator(selectedCard.url);
-        const closeButton = document.querySelector("button");
-        closeButton.addEventListener("click", (event) => {
+    //selezioniamo l'evento di click sulle varie card
+    allCard.forEach( (card, index) => {
+      card.addEventListener("click", (event) => {                           //quando viene cliccata una card:
+        activateOverlay.classList.remove("disactive");                      //appare overlay con immagine della card grande
+        stopBody.classList.add("hidden");                                   //viene bloccato lo scroll e i vari comportamenti del body
+        const selectedCard = posts.find(post => post.id === index + 1);     //recupero l'oggetto corrispondente alla card selezionata
+        activateOverlay.innerHTML = overlayGenerator(selectedCard.url);    //faccio creare l'overlay con l'url dell'immagine di questa card da inssrire nell'html
+        const closeButton = document.querySelector("button");              //seleziono il bottone appena creato
+        closeButton.addEventListener("click", (event) => {                 //quando clicco il bottone:
           event.preventDefault();
-          activateOverlay.classList.add("disactive");
+          activateOverlay.classList.add("disactive");                      //faccio chiudere l'overlay e riprendo tutti i normali comportamenti del body
           stopBody.classList.remove("hidden");
         });
       });
-    }
+    });
   })
   .catch((error) => {
     // codice da eseguire in caso di errore(mando errore in pagina)
     document.querySelector("body").innerHTML = error.message;
   });
 
+// FUNZIONI
 function cardGenerator(description, date, urlImage) {
   output = `       
         <div class="card">
-          <img src="./img/pin.svg" alt="" id="pin" />
+          <img src="./img/pin.svg" alt="" class="pin" />
           <figure>
             <img src="${urlImage}" alt="" />
           </figure>
           <figcaption>
-            <h2>${description}</h2>
+            <h2>${description.toUpperCase()}</h2>
             <p>${date}</p>
           </figcaption>
         </div>`;
